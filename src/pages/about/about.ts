1539cbfbaaa1438945b3aa630a185235a6c781a6
khimpat009 <khimpat.endaya09@gmail.com>
2017-11-23 14:24:07 +0800

    sms

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { SMS } from '@ionic-native/sms';
 
 @Component({
   selector: 'page-about',
@@ -7,8 +8,12 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private sms: SMS) {
 
+  }
+
+  textnow() {
+   this.sms.send('09957840689', 'Hello world!');
   }
 
 }
